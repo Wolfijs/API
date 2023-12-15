16b38f0d3732aca01b84c13f77f4007eb4353f9c
Wolfijs <83542115+Wolfijs@users.noreply.github.com>
2023-12-15 17:24:49 +0200

    Update orderaccepted.php

--- storage/orderaccepted.php
+++ storage/orderaccepted.php
@@ -1,46 +1,56 @@
 <?php
-// Allow requests from any origin
-header("Access-Control-Allow-Origin: *");
 
-// Allow the following methods
-header("Access-Control-Allow-Methods: POST, OPTIONS");
+class OrderUpdater {
+    private $conn;
 
-// Allow the following headers
-header("Access-Control-Allow-Headers: Content-Type");
-
-// Set content type to JSON
-header("Content-Type: application/json");
-
-include 'db.php';
-
-// Check if the request method is POST
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Get the JSON data from the request body
-    $data = json_decode(file_get_contents("php://input"));
-
-    // Check if the 'orderIds' property exists in the JSON data
-    if (isset($data->orderIds) && is_array($data->orderIds)) {
-        // Convert the array of order IDs to a comma-separated string
-        $orderIdsString = implode(',', $data->orderIds);
-
-        // Update the order status for the selected orders
-        $updateQuery = "UPDATE orders SET statuss = 'Accepted' WHERE id IN ($orderIdsString)";
-
-        if ($conn->query($updateQuery) === TRUE) {
-            $response = array('success' => true, 'message' => 'Order status updated successfully');
-        } else {
-            $response = array('success' => false, 'message' => 'Error updating order status: ' . $conn->error);
-        }
-    } else {
-        $response = array('success' => false, 'message' => 'Invalid request data');
+    public function __construct($conn) {
+        $this->conn = $conn;
     }
-} else {
-    $response = array('success' => false, 'message' => 'Invalid request method');
+
+    public function handleRequest() {
+        header("Access-Control-Allow-Origin: *");
+        header("Access-Control-Allow-Methods: POST, OPTIONS");
+        header("Access-Control-Allow-Headers: Content-Type");
+        header("Content-Type: application/json");
+
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            $this->processPostRequest();
+        } else {
+            $this->sendResponse(false, 'Invalid request method');
+        }
+
+        $this->conn->close();
+    }
+
+    private function processPostRequest() {
+        $data = json_decode(file_get_contents("php://input"));
+
+        if ($this->isValidRequestData($data)) {
+            $orderIdsString = implode(',', $data->orderIds);
+            $updateQuery = "UPDATE orders SET statuss = 'Accepted' WHERE id IN ($orderIdsString)";
+
+            if ($this->conn->query($updateQuery) === TRUE) {
+                $this->sendResponse(true, 'Order status updated successfully');
+            } else {
+                $this->sendResponse(false, 'Error updating order status: ' . $this->conn->error);
+            }
+        } else {
+            $this->sendResponse(false, 'Invalid request data');
+        }
+    }
+
+    private function isValidRequestData($data) {
+        return isset($data->orderIds) && is_array($data->orderIds);
+    }
+
+    private function sendResponse($success, $message) {
+        $response = array('success' => $success, 'message' => $message);
+        echo json_encode($response);
+    }
 }
 
-// Output the JSON response
-echo json_encode($response);
-
-// Close the database connection
-$conn->close();
+// Usage
+include 'db.php';
+$orderUpdater = new OrderUpdater($conn);
+$orderUpdater->handleRequest();
 ?>
